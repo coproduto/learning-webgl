cfe5fafe21b9cdb30565c3f8e6b7199334077087
Pedro Castilho <cstl.px@gmail.com>
2018-11-21 14:02:34 -0300

    Added color changing example

---
+++ part-1/changing-color.js
@@ -0,0 +1,31 @@
+const getWebGLContext = (canvas, onDetect, onFail) => {
+  const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+  if (gl && gl instanceof WebGLRenderingContext) {
+    return onDetect(gl);
+  } else {
+    return onFail();
+  }
+};
+
+const switchColor = (ctx) => {
+  ctx.clearColor(Math.random(), Math.random(), Math.random(), 1.0);
+  ctx.clear(ctx.COLOR_BUFFER_BIT);
+};
+
+window.addEventListener("load", function initWebGL (evt) {
+  window.removeEventListener(evt.type, initWebGL, false);
+  const theCanvas = document.getElementById("the-canvas");
+  
+  getWebGLContext(
+    document.getElementById("the-canvas"),
+    (ctx) => {
+      ctx.viewport(0, 0, ctx.drawingBufferWidth, ctx.drawingBufferHeight);
+      ctx.clearColor(0, 0.5, 0.0, 1.0);
+      ctx.clear(ctx.COLOR_BUFFER_BIT);
+
+      theCanvas.addEventListener("click", () => switchColor(ctx), false);
+      console.log("Ready");
+    },
+    () => console.error("ERROR: No WebGL support detected.")
+  );
+});
